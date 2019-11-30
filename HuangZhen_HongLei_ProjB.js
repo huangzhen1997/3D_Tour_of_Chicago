ea134a859e7379deeea60b10a42239bd093eb189
leohl1997 <leih5900@gmail.com>
2019-11-30 14:39:47 -0600

    Update HuangZhen_HongLei_ProjB.js

--- HuangZhen_HongLei_ProjB.js
+++ HuangZhen_HongLei_ProjB.js
@@ -100,7 +100,7 @@ function main() {
   }
 
   // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.5, 0.5, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
 	// NEW!! Enable 3D depth-test when drawing: don't over-draw at any pixel
 	// unless the new Z value is closer to the eye than the old one..
@@ -1069,11 +1069,11 @@ function makeGroundGrid() {
 // Create a list of vertices that create a large grid of lines in the x,y plane
 // centered at the origin.  Draw this shape using the GL_LINES primitive.
 
-	var xcount = 100;			// # of lines to draw in x,y to make the grid.
-	var ycount = 100;
-	var xymax	= 100.0;			// grid size; extends to cover +/-xymax in x and y.
- 	var xColr = new Float32Array([1.0, 1.0, 0.0]);	// bright yellow
- 	var yColr = new Float32Array([0.0, 1.0, 0.0]);	// bright green.
+	var xcount = 200;			// # of lines to draw in x,y to make the grid.
+	var ycount = 200;
+	var xymax	= 80.0;			// grid size; extends to cover +/-xymax in x and y.
+    var xColr = Math.random()*0.5;  
+    var yColr = Math.random() * 0.5; 
 
 	// Create an (global) array to hold this ground-plane's vertices:
 	gndVerts = new Float32Array(floatsPerVertex*2*(xcount+ycount));
@@ -1088,17 +1088,17 @@ function makeGroundGrid() {
 			gndVerts[j  ] = -xymax + (v  )*xgap;	// x
 			gndVerts[j+1] = -xymax;								// y
 			gndVerts[j+2] = 0.0;									// z
-			gndVerts[j+3] = 1.0;									// w.
+			//gndVerts[j+3] = 1.0;									// w.
 		}
 		else {				// put odd-numbered vertices at (xnow, +xymax, 0).
 			gndVerts[j  ] = -xymax + (v-1)*xgap;	// x
 			gndVerts[j+1] = xymax;								// y
 			gndVerts[j+2] = 0.0;									// z
-			gndVerts[j+3] = 1.0;									// w.
+			//gndVerts[j+3] = 1.0;									// w.
 		}
-		gndVerts[j+4] = xColr[0];			// red
-		gndVerts[j+5] = xColr[1];			// grn
-		gndVerts[j+6] = xColr[2];			// blu
+		gndVerts[j+3] = xColr;			// red
+		gndVerts[j+4] = yColr;			// grn
+		gndVerts[j+5] = 1;			// blu
 	}
 	// Second, step thru y values as wqe make horizontal lines of constant-y:
 	// (don't re-initialize j--we're adding more vertices to the array)
@@ -1107,17 +1107,17 @@ function makeGroundGrid() {
 			gndVerts[j  ] = -xymax;								// x
 			gndVerts[j+1] = -xymax + (v  )*ygap;	// y
 			gndVerts[j+2] = 0.0;									// z
-			gndVerts[j+3] = 1.0;									// w.
+			//gndVerts[j+3] = 1.0;									// w.
 		}
 		else {					// put odd-numbered vertices at (+xymax, ynow, 0).
 			gndVerts[j  ] = xymax;								// x
 			gndVerts[j+1] = -xymax + (v-1)*ygap;	// y
 			gndVerts[j+2] = 0.0;									// z
-			gndVerts[j+3] = 1.0;									// w.
+			//gndVerts[j+3] = 1.0;									// w.
 		}
-		gndVerts[j+4] = yColr[0];			// red
-		gndVerts[j+5] = yColr[1];			// grn
-		gndVerts[j+6] = yColr[2];			// blu
+		gndVerts[j+3] = xColr;			// red
+		gndVerts[j+4] = yColr;			// grn
+		gndVerts[j+5] = 1;			// blu
 	}
 }
 
